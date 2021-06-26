948984525569e57ae18371185699fb47988beeb8
Daniel Smanioto <daniel.smanioto@gmail.com>
2021-06-26 15:53:41 -0300

    creating project

---
+++ src/test/java/com/danielsmanioto/javaaws/JavaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsmanioto.javaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
